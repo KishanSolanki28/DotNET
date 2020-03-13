d329db2d4237b4ef735d30df2aba79b1fe10426e
kalpesh solanki <kalpesh solanki>
2020-03-13 09:19:36 +0530

    Commit

---
+++ Program2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, country;
+            do
+            {
+                Console.WriteLine("Hello! Whats your name?");
+                name = Console.ReadLine();
+            } while (!Regex.IsMatch(name,@"^[a-zA-Z]+$"));
+            do
+            {
+                Console.WriteLine("Where are you from?");
+                country = Console.ReadLine();
+            } while (!Regex.IsMatch(country,@"^[a-zA-Z]+$"));
+
+            Console.WriteLine("Hello " + name + "from country " + country);
+            Console.Read();
+        }
+    }
+}
